6149cb501adfe23a3e8cb0b22038c08d1618fcb8
pieszynski <git@pieszynski.com>
2014-11-30 16:44:45 +0100

    Dodanie GRUNTa do kompilacji i startu projektu

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            build : {
+                force : true,
+                src : ['www/js/build/**']
+            },
+            live : {
+                force : true,
+                src : ["www/js/built-app.js"]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: {
+                    except: ['jQuery', 'Backbone', 'angular']
+                }
+            },
+            live: {
+                files: {
+                    'www/js/build/source.min.js': ['www/js/build/source.js']
+                }
+            }
+        },
+        concat : {
+            dataAndIndex : {
+                src : ['www/js/data.js', 'www/js/index.js'],
+                dest : 'www/js/build/source.js'
+            },
+            vendors : {
+                src : ['www/js/jqlite.1.1.1.min.js','www/js/angular.min.js','www/js/angular-touch.min.js','www/js/angular-sanitize.min.js'],
+                dest : 'www/js/build/vendors.min.js'
+            },
+            dev : {
+                src : ['www/js/build/vendors.min.js', 'www/js/build/source.js'],
+                dest : 'www/js/built-app.js'
+            },
+            live : {
+                src : ['www/js/build/vendors.min.js', 'www/js/build/source.min.js'],
+                dest : 'www/js/built-app.js'
+            }
+        },
+        shell: {
+            cordovaBrowser: {
+                command: 'cordova build browser && cordova serve'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.registerTask('cls', ['clean']);
+
+    grunt.registerTask('dev', [
+        'clean',
+        'concat:dataAndIndex',
+        'concat:vendors',
+        'uglify',
+        'concat:dev',
+        'clean:build',
+        'shell:cordovaBrowser'
+        ]);
+
+};
