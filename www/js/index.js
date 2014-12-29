d2811a84191677bcf8e126992460f5e282bc069a
pieszynski <git@pieszynski.com>
2014-12-29 17:01:57 +0100

    Zapis zaznaczania gwiazdki do ustawień

--- www/js/index.js
+++ www/js/index.js
@@ -40,9 +40,16 @@
         self.data = window.Data;
         self.learn = [];
         self.btnTexts = ['znaczenie', 'następne'];
+        self.applicationModes = ['allIrregularVerbs', 'starredIrregularVerbs'];
 
         self.title = 'Deutsch mit Spaß lernen';
         self.chevron = '<span class="md md-chevron-right"></span>';
+        self.defaultApplicationMode = self.applicationModes[0];
+
+        self.appSettings = {
+            applicationMode : self.defaultApplicationMode,
+            starredIrregularVerbs : []
+        };
 
         self.isMenuHidden = true;
         self.menuButtonButtonClass = 'md-menu menu-button menu-button-hidden';
@@ -77,11 +84,34 @@
 
             for(var i = 0; i < self.data.length; i++) {
 
-                var obj = {pl : self.data[i].pl, de : self.data[i].de};
-                self.learn.push(obj);
+                var obj = {pl : self.data[i].pl, de : self.data[i].de, hasStar : false};
 
+                // sprawdzenie czy obiektu nie ma na liście oznaczonych gwiazdką
+                obj.hasStar = !(-1 == self.getStarredIndex(obj));
+
+                self.learn.push(obj);
             }
         }
+
+        self.getStarredIndex = function (wordModel) {
+
+            // sprawdzenie istnienia słówka na liście
+            var wordExistsAtIdx = -1;
+            if (undefined === wordModel || null == wordModel || undefined === wordModel.pl)
+                return wordExistsAtIdx;
+
+            for (var i = 0; i < self.appSettings.starredIrregularVerbs.length; i++) {
+
+                if (wordModel.pl == self.appSettings.starredIrregularVerbs[i]) {
+
+                    wordExistsAtIdx = i;
+                    break;
+                }
+            }
+
+            return wordExistsAtIdx;
+        };
+
 
         self.onBackButton = function () {
 
@@ -103,6 +133,66 @@
                     $scope.$applyAsync();
 
             return response;
+        };
+
+        self.updateAppSettingsFromSaveState = function (appSettings) {
+
+            // aktualizacja obiektu ustawień dostępnego dla kontrolera
+
+            if (undefined === appSettings)
+                return;
+
+            self.appSettings = appSettings;
+        };
+
+        self.readAllAppSettings = function () {
+
+            // odczytanie ustawień
+            var sAppSettings = window.localStorage.getItem('appSettings');
+
+            if (undefined === sAppSettings || null == sAppSettings)
+                return;
+
+            var appSettings = JSON.parse(sAppSettings);
+
+            // aktualizacja obiektu ustawień
+            self.updateAppSettingsFromSaveState(appSettings);
+        };
+
+        self.saveAllAppSettings = function () {
+
+            var sAppSettings = JSON.stringify(self.appSettings);
+
+            window.localStorage.setItem('appSettings', sAppSettings);
+        };
+
+        self.setAppSettingsStar = function (wordModel, hasStar) {
+
+            // sprawdzenie istnienia słówka na liście
+            var wordExistsAtIdx = self.getStarredIndex(wordModel);
+
+            // usunięcie lub dodanie słówka do listy oznaczonych gwiazdką
+            if (hasStar) {
+
+                // dodanie do listy
+                if (-1 == wordExistsAtIdx) {
+
+                    self.appSettings.starredIrregularVerbs.push(wordModel.pl);
+                }
+            } else {
+
+                // usunięcie z listy
+                if (0 <= wordExistsAtIdx) {
+
+                    self.appSettings.starredIrregularVerbs.splice(wordExistsAtIdx, 1);
+                }
+            }
+
+            // zapis obiektu ustawień
+            self.saveAllAppSettings();
+
+            // wizualna podmiana zaznaczenia gwiazdki
+            wordModel.hasStar = hasStar;
         };
 
         self.menuClick = function (doHideMenuOverride) {
@@ -158,8 +248,36 @@
                 return;
 
             }
-
         }
+
+        self.changeApplicationMode = function (modeName) {
+
+            // akcja domyślna
+            if (!modeName)
+                modeName = self.appSettings.applicationMode;
+
+            // wyczyszczenie pól
+            self.shown = undefined;
+            self.prev = undefined;
+
+            // wyczyszczenie danych dla pól
+            if (0 < self.learn.length)
+                self.learn.splice(0, self.learn.length);
+
+            // pobranie pierwszego elementu
+            self.tap();
+
+            // odświeżenie interfejsu
+            $scope.$applyAsync();
+        };
+
+        self.starClick = function (evt, viewElementModel) {
+
+            evt.stopPropagation();
+
+            // zapisanie wartości w ustawieniach
+            self.setAppSettingsStar(viewElementModel.item, !viewElementModel.item.hasStar);
+        };
 
         self.changeToSearchMode = function (isOn) {
 
@@ -173,6 +291,9 @@
 
             self.shown = {item:delem, text:delem.pl, info:delem.info, state:1};
             self.nextBtn = self.btnTexts[self.shown.state];
+
+            // sprawdzenie oznaczenie gwiazdki następuje dopiero po wybraniu słówka
+            self.shown.item.hasStar = !(-1 == self.getStarredIndex(self.shown.item));
 
             self.changeToSearchMode(false);
         }
@@ -193,6 +314,7 @@
                         pl : self.data[idx].pl,
                         de : self.data[idx].de,
                         info : self.toChevron(self.data[idx].de),
+                        hasStar : false,
                         visible : true
                     };
                     self.searchResults.push(delem);
@@ -224,7 +346,8 @@
 
 
         window.registerBack(self.onBackButton);
-        self.tap();
+        self.readAllAppSettings();
+        self.changeApplicationMode();
         self.initSearchObject();
 
     }]);
